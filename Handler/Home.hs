b334a23d17024419135a1988a28764890fc2917e
Nicholas Montaño <montanonic@gmail.com>
2015-12-22 01:59:03 -0800

    Create Homepage prototype. Document Home handlers.
    This prototype is currently functional from backend to frontend, though it is
    ugly as sin (of course). Adding a <script> tag to the appropriate stage will be
    all that's necessary to bring us right into the JS App from this prototype, so
    we're basically ready to go at this point.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,13 +1,47 @@
 module Handler.Home where
 
 import Import
+import Data.Time.Calendar()
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
+-- | This is for prototyping, and will require adding server-side and/or
+-- client-side validation before it should be allowed for production.
 --
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
+-- NOTE: This is a PARTIAL FUNCTION, and WILL cause app crashes in the event of
+-- failure. THIS SHOULD NOT BE INCLUDED IN A REAL WEBSITE AS IS.
+--
+-- An ideal solution to the partial function problem would be to make this
+-- and runFormPost return Maybe FormResults. However, my many attempts at that
+-- failed, which is why I'm resorting to an unsafe function right now.
+profileForm :: Maybe UserId -> Form Profile
+profileForm muid = renderDivs $ Profile
+    <$> pure uid -- user
+    <*> areq textField "First Name" Nothing
+    <*> areq textField "Last Name" Nothing
+    <*> areq dayField "Birthday" Nothing
+    <*> areq (radioField optionsEnum) "Gender" Nothing
+    <*> lift (liftIO getCurrentTime) -- createdAt
+    <*> pure Nothing -- updatedAt
+  where
+    Just uid = muid -- circumvents pattern-match error.
+
 getHomeR :: Handler Html
-getHomeR = defaultLayout $(widgetFile "homepage")
+getHomeR = do
+    -- maybe get a user's authenticated identity
+    muid <- maybeAuthId
+    -- maybe find a user's Profile entity connected to their UserId
+    mprofile <- maybe (pure Nothing)
+        (runDB . getBy . UniqueProfile) muid
+
+    (formWidget, enctype) <- generateFormPost $ profileForm muid
+    defaultLayout $(widgetFile "homepage")
+
+postHomeR :: Handler Html
+postHomeR = do
+    muid <- maybeAuthId
+    ((res, _), _) <- runFormPost $ profileForm muid
+    case res of
+        FormMissing -> setMessage "We didn't get any information. Please try\
+            \ again."
+        FormFailure fs -> setMessage [shamlet|#{show fs}|]
+        FormSuccess profile -> runDB $ insert_ profile
+    redirect HomeR
